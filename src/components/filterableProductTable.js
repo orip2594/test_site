7a19368036c7def4e67c9e0d3caaef99ac2fed38
ori <oripascal1996@gmail.com>
2024-05-21 16:04:11 +0300

    new branch

---
+++ src/components/filterableProductTable.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react';
+import SearchBar from './searchBar';
+import ProductTable from './ProductTable';
+
+
+  
+
+export default function FilterableProductTable({ products }) {
+
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+  
+  
+    return (
+    <div>
+        <SearchBar
+            filterText={filterText} 
+            inStockOnly={inStockOnly} 
+            onFilterTextChange={setFilterText} 
+            onInStockOnlyChange={setInStockOnly} />
+
+        <ProductTable 
+            products={products} 
+            filterText={filterText}
+            inStockOnly={inStockOnly} />
+    </div>
+  )
+}
